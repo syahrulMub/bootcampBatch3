0cc739f2c9e671d1d3adc07065bb49a2959dd651
Syahrul Mubarok <syahrul.mubarrok4@gmail.com>
2023-07-04 17:47:51 +0700

    para pemain

--- day2/PemainSepakBola/Program.cs
+++ day2/PemainSepakBola/Program.cs
@@ -6,11 +6,17 @@ class Program
 {
     static void Main()
     {
-        Player Player = new Player("Benzema", 34, "Al Ittihad");
-        string NamaPlayer = Player.GetName();
-        int UmurPlayer = Player.GetUmur();
-        string ClubPlayer = Player.GetClub();
-
-        Console.WriteLine($"Nama : {NamaPlayer} \nUmur : {UmurPlayer} \nClub : {ClubPlayer}");
+        List<Player> Players = new List<Player>();
+        Players.Add(new Player("Benzema", 34, "Al Ittihad"));
+        Players.Add(new Player("Camavinga", 22, "Real Madrid"));
+        Players.Add(new Player("Phil Foden", 23, "Man City"));
+        
+        foreach (Player player in Players)
+        {
+        string NamaPlayer = player.GetName();
+        int UmurPlayer = player.GetUmur();
+        string ClubPlayer = player.GetClub();
+        Console.WriteLine($"Nama : {NamaPlayer} \nUmur : {UmurPlayer} \nClub : {ClubPlayer}\n");
+        }
     }
 }
