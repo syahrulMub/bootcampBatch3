ca5c895900f9790ad327c3a7d38d70646e2a5986
Syahrul Mubarok <syahrul.mubarrok4@gmail.com>
2023-07-06 01:36:02 +0700

    menambahkan fungsi animal dengan kelas parent dan child

---
+++ day3/Animal/MyAnimal.cs
@@ -0,0 +1,34 @@
+namespace MyAnimal;
+class Animal
+{
+    public string? name { get; set; }
+    public int? age { get; set; }
+
+    public void GetInfo()
+    {
+        Console.WriteLine("Nama : " + name);
+        Console.WriteLine("Umur : " + age);
+    }
+    public string Eat(string makanan)
+    {
+        return $"{name} makanannya adalah {makanan}";
+    }
+
+}
+class Cat : Animal
+{
+    public string catSound = "Meow";
+    public void CatSound()
+    {
+        Console.WriteLine(name + " sound : " + catSound);
+    }
+}
+
+class Bird : Animal
+{
+    public string birdSound = "Assalamualaikum";
+    public void BirdSound()
+    {
+    Console.WriteLine(name + " sound : " + birdSound );
+    }
+}
---
+++ day3/Animal/Program.cs
@@ -0,0 +1,28 @@
+﻿﻿using MyAnimal;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		//membuat objek baru dari class child
+		Cat Leo = new Cat();
+		Bird Jago = new Bird();
+
+		//membuat properti child dari parent
+		Leo.name = "Leo";
+		Leo.age = 2;
+		Jago.name = "Jago";
+		Jago.age = 1;
+
+		//fungsi untuk memanggil deskripsi
+		 Leo.GetInfo();
+		 Leo.CatSound();
+		 Console.WriteLine(Leo.Eat("wiskas"));
+		 Jago.GetInfo();
+		 Jago.BirdSound();
+		 Console.WriteLine(Jago.Eat("biji-biji"));
+
+
+
+	}
+}
