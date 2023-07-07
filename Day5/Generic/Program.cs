83310692d4e98431fc64e72a15e29e4601d4ba9e
syahrulMub <syahrul.mubarrok4@gmail.com>
2023-07-07 11:23:25 +0700

    day 5

---
+++ Day5/Generic/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class MyInt<T>
+{
+    private T _value;
+    public MyInt(T value)
+    {
+        _value = value;
+    }
+    public T GetValue() { return _value; }
+}
+
+class MyString<T>
+{
+    private T _value;
+    public MyString(T value)
+    {
+        _value = value;
+    }
+    public T GetValue() { return _value; }
+}
+
+class MyGeneric<T1, T2>
+{
+    private T1 _value1;
+    private T2 _value2;
+    public MyGeneric(T1 value1, T2 value2)
+    {
+        _value1 = value1;
+        _value2 = value2;
+    }
+    public T1 GetValue1() { return _value1; }
+    public T2 GetValue2() { return _value2; }
+
+    public void PrintInt(T1 value1)
+    {
+        Console.WriteLine(value1);
+    }
+    public void PrintString(T2 value2)
+    {
+        Console.WriteLine(value2);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyInt<int> myInt = new MyInt<int>(10);
+        Console.WriteLine(myInt.GetValue());
+        
+        MyString<string> myString = new MyString<string>("Hello");
+        Console.WriteLine(myString.GetValue());
+
+        int a = 60;
+        string pernyataan = "Besok libur";
+        double Phi = 3.14159;
+
+        PrintValue(a);
+        PrintValue(pernyataan);
+        PrintValue(Phi);
+
+        MyGeneric<int, string> myGeneric = new MyGeneric<int, string>(1, "besok");
+        myGeneric.PrintInt(myGeneric.GetValue1());
+        myGeneric.PrintString(myGeneric.GetValue2());
+
+        Console.ReadLine();
+    }
+
+    static void PrintValue<T>(T value)
+    {
+        Console.WriteLine(value);
+    }
+}
---
+++ Day5/MutableAndImmutable/Program.cs
@@ -0,0 +1,61 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 5;
+        int b = a;
+        b = 2;
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        //class Parent 
+        Parent P = new Parent(5);
+        Parent C = P;
+        C.x = 2;
+        Console.WriteLine(P.x);
+        Console.WriteLine(C.x);
+
+        MyClass obj1 = new MyClass(10);
+        obj1.value = 10;
+
+        MyClass obj2 = obj1;
+        obj2.value = 20;
+        Console.WriteLine(obj1.value);
+        Console.WriteLine(obj2.value);
+
+        MyStruct obj3 = new MyStruct(10);
+        obj3.value = 10;
+        
+        MyStruct obj4 = obj3;
+        obj4.value = 20;
+        Console.WriteLine(obj3.value);
+        Console.WriteLine(obj4.value);
+    }
+}
+
+class Parent
+{
+    public int x;
+    public Parent(int x)
+    {
+        this.x = x;
+    }
+}
+
+public class MyClass
+{
+    public int value;
+    public MyClass(int value)
+    {
+        this.value = value;
+    }
+
+}
+
+public struct MyStruct
+{
+    public int value;
+    public MyStruct(int value)
+    {
+        this.value = value;
+    }
+}
