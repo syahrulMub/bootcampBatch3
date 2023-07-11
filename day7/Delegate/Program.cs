8d1cd11620c4d72cbc936e7f8158cacb3e9bcb6c
syahrulMub <syahrul.mubarrok4@gmail.com>
2023-07-11 12:03:09 +0700

    day 7

---
+++ day7/Delegate/Program.cs
@@ -0,0 +1,21 @@
+﻿public delegate void MyDelegate();
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        MyDelegate Delegate = sayHello;
+        Delegate();
+        Delegate += sayHi;
+        Delegate();
+    }
+
+    static void sayHello()
+    {
+        Console.WriteLine("Hello");
+    }
+    static void sayHi()
+    {
+        Console.WriteLine("Hi");
+    }
+}
---
+++ day7/Event/Program.cs
@@ -0,0 +1,15 @@
+﻿public delegate void MyDelegate();
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        MyDelegate Delegate = sayHello;
+        Delegate();
+    }
+
+    static void sayHello()
+    {
+        Console.WriteLine("Hello");
+    }
+}
