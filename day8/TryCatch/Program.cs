904e4f1fa084d149e5a09e2db56133e55986763e
SyahrulMub <syahrul.mubarrok4@gmail.com>
2023-07-13 15:17:36 +0700

    tryCatch

--- day8/TryCatch/Program.cs
+++ day8/TryCatch/Program.cs
@@ -3,17 +3,24 @@ class Program
 {
     static void Main(string[] args)
     {
+        Pecahan pecahan1 = new Pecahan(9,4);
+        Pecahan pecahan2 = new Pecahan(3,9);
+        Pecahan pecahan3 = new Pecahan(3,0);
         try
         {
-            Pecahan pecahan1 = new Pecahan(9,4);
-            Pecahan pecahan2 = new Pecahan(3,8);
-            Pecahan pecahan3 = new Pecahan(3,8);
+            if (pecahan1.penyebut == 0 || pecahan2.penyebut == 0 || pecahan3.penyebut == 0)
+            {
+                throw new DivideByZeroException("Penyebut 0 tidak dapat di Operasikan");
+            }
+            else 
+            {
             Pecahan hasilJumlah = pecahan1 + pecahan2;
             hasilJumlah.Simplify();
             Pecahan hasilKurang = hasilJumlah - pecahan3;
             hasilKurang.Simplify();
             Console.WriteLine($"hasil Penjumlahan : {hasilJumlah.pembilang}/{hasilJumlah.penyebut}");
             Console.WriteLine($"hasil Pengurangan : {hasilKurang.pembilang}/{hasilKurang.penyebut}");
+            }
         }
         catch (DivideByZeroException e)
         {
@@ -21,7 +28,7 @@ class Program
         }
         finally
         {
-            Console.WriteLine("Pecahan dengan pembagi 0 tidak dapat dioperasikan");
+            Console.WriteLine("Warning !!! Pecahan dengan pembagi 0 tidak dapat dioperasikan");
         }
     }
 }
