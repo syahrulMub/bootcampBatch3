20f464c9a7db05e4b89c1acdf9b63a022a71864e
SyahrulMub <syahrul.mubarrok4@gmail.com>
2023-07-21 07:51:09 +0700

    CLASS DIAGRAM

--- Thread/Program.cs
+++ Thread/Program.cs
@@ -1,32 +1,206 @@
-﻿// using System;
-// using System.Threading;
+﻿﻿using System;
+using System.Collections.Generic;
 
-// class Program
-// {
-//     static void Main()
-//     {
-//         // Membuat objek Thread dan memberikan metode yang akan dijalankan di dalamnya
-//         Thread t1 = new Thread(CountNumbers);
-        
-//         // Memulai thread
-//         t1.Start();
-//         // Melakukan tugas lain di thread utama
-//         for (int i = 1; i <= 5; i++)
-//         {
-//             Console.WriteLine("Tugas lain: " + i);
-//             Thread.Sleep(500); // Memberikan jeda setengah detik
-//         }
-        
-//         // Menunggu thread lain selesai
-//         t1.Join();
-//         Console.WriteLine("Selesai.");
-//     }
-//     static void CountNumbers()
-//     {
-//         for (int i = 1; i <= 5; i++)
-//         {
-//             Console.WriteLine("Thread: " + i);
-//             Thread.Sleep(1000); // Memberikan jeda satu detik
-//         }
-//     }
-// }
+const int mine = -1;
+Random random = new();
+(int Value, bool Visible)[,] board;
+
+Console.WriteLine("Minesweeper");
+Console.WriteLine();
+int selectedWidth = GetIntegerInput($"Enter board width (10-{Math.Min(Console.LargestWindowWidth, 50)}): ", 10, Math.Min(Console.LargestWindowWidth, 50));
+int selectedHeight = GetIntegerInput($"Enter board height (10-{Math.Min(Console.LargestWindowHeight, 50)}): ", 10, Math.Min(Console.LargestWindowHeight, 50));
+double mineRatio = GetMineRatio("Enter mine ratio (example: 0.1): ");
+int mineCount = (int)(selectedWidth * selectedHeight * mineRatio);
+if (OperatingSystem.IsWindows())
+{
+	Console.WindowHeight = selectedHeight;
+	Console.WindowWidth = selectedWidth;
+}
+(int Column, int Row) = (selectedWidth / 2, selectedHeight / 2);
+GenerateBoard();
+Console.Clear();
+RenderBoard();
+int height = Console.WindowHeight;
+int width = Console.WindowWidth;
+while (true)
+{
+	if (Console.WindowHeight != height || Console.WindowWidth != width)
+	{
+		RenderBoard();
+	}
+	Console.SetCursorPosition(Column, Row);
+	switch (Console.ReadKey(true).Key)
+	{
+		case ConsoleKey.UpArrow:    Row = Math.Max(Row - 1, 0); break;
+		case ConsoleKey.DownArrow:  Row = Math.Min(Row + 1, selectedHeight - 1); break;
+		case ConsoleKey.LeftArrow:  Column = Math.Max(Column - 1, 0); break;
+		case ConsoleKey.RightArrow: Column = Math.Min(Column + 1, selectedWidth - 1); break;
+		case ConsoleKey.Enter:
+			if (!board[Column, Row].Visible)
+			{
+				if (board[Column, Row].Value == mine)
+				{
+					for (int column = 0; column < selectedWidth; column++)
+					{
+						for (int row = 0; row < selectedHeight; row++)
+						{
+							board[column, row].Visible = true;
+						}
+					}
+					RenderBoard();
+					Console.SetCursorPosition(0, selectedHeight - 1);
+					Console.Write("You Lose. Press Enter To Exit...");
+					Console.ReadLine();
+					Console.Clear();
+					Console.Write("Minesweeper was closed.");
+					return;
+				}
+				else if (board[Column, Row].Value == 0)
+				{
+					Reveal(Column, Row);
+					RenderBoard();
+				}
+				else
+				{
+					board[Column, Row].Visible = true;
+					RenderBoard();
+				}
+				int visibleCount = 0;
+				for (int column = 0; column < selectedWidth; column++)
+				{
+					for (int row = 0; row < selectedHeight; row++)
+					{
+						if (board[column, row].Visible)
+						{
+							visibleCount++;
+						}
+					}
+				}
+				if (visibleCount == selectedWidth * selectedHeight - mineCount)
+				{
+					Console.SetCursorPosition(0, selectedHeight - 1);
+					Console.Write("You Win. Press Enter To Exit...");
+					Console.ReadLine();
+					Console.Clear();
+					Console.Write("Minesweeper was closed.");
+					return;
+				}
+			}
+			break;
+		case ConsoleKey.Escape:
+			Console.Clear();
+			Console.Write("Minesweeper was closed.");
+			return;
+	}
+}
+
+IEnumerable<(int Row, int Column)> AdjacentTiles(int column, int row)
+{
+	//    A B C
+	//    D + E
+	//    F G H
+
+	/* A */ if (row > 0 && column > 0) yield return (row - 1, column - 1);
+	/* B */ if (row > 0) yield return (row - 1, column);
+	/* C */ if (row > 0 && column < selectedWidth - 1) yield return (row - 1, column + 1);
+	/* D */ if (column > 0) yield return (row, column - 1);
+	/* E */ if (column < selectedWidth - 1) yield return (row, column + 1);
+	/* F */ if (row < selectedHeight - 1 && column > 0) yield return (row + 1, column - 1);
+	/* G */ if (row < selectedHeight - 1) yield return (row + 1, column);
+	/* H */ if (row < selectedHeight - 1 && column < selectedWidth - 1) yield return (row + 1, column + 1);
+}
+
+int GetIntegerInput(string prompt, int min, int max)
+{
+	int inputValue;
+	Console.Write(prompt);
+	while (!int.TryParse(Console.ReadLine(), out inputValue) || inputValue < min || max < inputValue)
+	{
+		Console.WriteLine("Invalid Input. Try Again...");
+		Console.Write(prompt);
+	}
+	return inputValue;
+}
+
+double GetMineRatio(string prompt)
+{
+	double inputValue;
+	Console.Write(prompt);
+	while (!double.TryParse(Console.ReadLine(), out inputValue) || (mineCount = (int)(selectedWidth * selectedHeight * inputValue)) < 0 || mineCount > selectedHeight * selectedWidth)
+	{
+		Console.WriteLine("Invalid Input. Try Again...");
+		Console.Write(prompt);
+	}
+	return inputValue;
+}
+
+void GenerateBoard()
+{
+	board = new (int Value, bool Visible)[selectedWidth, selectedHeight];
+	var coordinates = new List<(int Row, int Column)>();
+	for (int column = 0; column < selectedWidth; column++)
+	{
+		for (int row = 0; row < selectedHeight; row++)
+		{
+			coordinates.Add((column, row));
+		}
+	}
+	for (int i = 0; i < mineCount; i++)
+	{
+		int randomIndex = random.Next(0, coordinates.Count);
+		(int column, int row) = coordinates[randomIndex];
+		coordinates.RemoveAt(randomIndex);
+		board[column, row] = (mine, false);
+		foreach (var tile in AdjacentTiles(column, row))
+		{
+			if (board[tile.Column, tile.Row].Value != mine)
+			{
+				board[tile.Column, tile.Row].Value++;
+			}
+		}
+	}
+}
+
+char Render(int value) => value switch
+{
+	mine => '@',
+	0 => ' ',
+	1 => '1',
+	2 => '2',
+	3 => '3',
+	4 => '4',
+	5 => '5',
+	6 => '6',
+	7 => '7',
+	8 => '8',
+	_ => throw new NotImplementedException(),
+};
+
+void RenderBoard()
+{
+	for (int row = 0; row < selectedHeight; row++)
+	{
+		for (int column = 0; column < selectedWidth; column++)
+		{
+			Console.SetCursorPosition(column, row);
+			Console.Write(board[column, row].Visible
+				? Render(board[column, row].Value)
+				: '█');
+		}
+	}
+}
+
+void Reveal(int column, int row)
+{
+	board[column, row].Visible = true;
+	if (board[column, row].Value == 0)
+	{
+		foreach (var (r, c) in AdjacentTiles(column, row))
+		{
+			if (!board[c, r].Visible)
+			{
+				Reveal(c, r);
+			}
+		}
+	}
+}
--- day12/Program.cs
+++ day12/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿classDiagram
+
+Player --> IPlayer
+Player"1" -- "1"RackScribble
+RackScribble -- Letter
+Board --> IBoard
+Square -- Board
+Letter -- Square
+Letter"ordered"-- Word
+Bag "1"--"ordered (0 - 7)" RackScribble 
+Word  "string"-- Dictionary
+  class IPlayer {
+    << interface>>
+    + int GetId();
+    + string GetName();
+    + void SetID();  
+    + void SetName();
+
+  }
+  class Player {
+    - int _playerID
+    - string _playerName  
+    + int GetId(): int
+    + string GetName(): string
+    + void SetID (int playerId);
+    + void SetName (string playerName);
+  }
+   class IBoard {
+    <<interface>>
+    + int GetSquare();
+  }
+   class Square{
+    + bool IsOccupied() : bolean
+    + int GetLetterScore() : int
+    + string PlaceLatter() : string
+   }
+  class Board {
+    - int _square;
+    + int GetSquare(x : 15 y: 15) : Square
+  }
+  class Letter{
+    + char;
+    + Score -readOnly-
+    + stirng SetWord(char char) : string
+  }
+  class Word{
+    - int score 
+    + starx [1-15]
+    + endx [1-15]
+    + stary [1-15]
+    = endy [1-15]
+  }
+  class Dictionary{
+    lookUp( W : Word) : boolean
+  }
+   class RackScribble{
+    + char[7] rack ;
+    + AddChar(c : char);
+    + RemoveChar( c : char); 
+   }
+   class Bag{
+    - bagSize [0 - 100]
+    + bool IsEmpety() : Boolean
+    + GiveLetters(int EmpetyRack) : set
+   }
+
+  
